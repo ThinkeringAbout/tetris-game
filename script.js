6d4558c6e42399bd12f746a742bbebe5a3d01026
ThinkeringAbout <104573860+ThinkeringAbout@users.noreply.github.com>
2022-08-01 12:44:47 +0300

    some prototype

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ let gameArray = [];
 gameArray.length = 180;
 gameArray.fill(0);
 
+let shapeArray = [];
+let zShape = [0, -1, 11, 10];
+let reversezShape = [0, 1, 9, 10];
+let cubeShape = [0, 1, 10, 11];
+shapeArray.push(zShape, reversezShape, cubeShape);
+
 let i = 0;
 let offset = 25;
+
+let active = cubeShape;
 
 for (let x = 0; x < 18; x++) {
     cells[x*10].style.backgroundColor = "green";
@@ -16,26 +24,36 @@ for (let x = 0; x < 10; x++) {
     cells[x+170].style.backgroundColor = "green";
 }
 
+function change() {
+  let res = Math.floor(Math.random() * shapeArray.length);
+  return shapeArray[res];
+}
+
 
 function move() {
-    gameArray[offset + i*10] = 1;
-    gameArray[offset + i*10 - 1] = 1;
-    gameArray[offset + i*10 + 11] = 1;
-    gameArray[offset + i*10 + 10] = 1;
-    
-    
+    gameArray[offset + i*10 + active[0]] = 1;
+    gameArray[offset + i*10 + active[1]] = 1;
+    gameArray[offset + i*10 + active[2]] = 1;
+    gameArray[offset + i*10 + active[3]] = 1;
+
+
     i++;
-    gameArray[offset + (i-2)*10] = 0;
-    gameArray[offset + (i-2)*10 - 1] = 0;
-    gameArray[offset + (i-2)*10 + 11] = 0;
-    
-    
-    if (cells[offset + i*10].style.backgroundColor == "green" || cells[offset + i*10].style.backgroundColor == "red" || 
-    cells[offset + i*10 - 1].style.backgroundColor == "green" || cells[offset + i*10 - 1].style.backgroundColor == "red" || 
-    cells[offset + i*10 + 11].style.backgroundColor == "green" || cells[offset + i*10 + 11].style.backgroundColor == "red" ||
-    cells[offset + i*10 + 10].style.backgroundColor == "green" || cells[offset + i*10 + 10].style.backgroundColor == "red") {
+    gameArray[offset + (i-2)*10 + active[0]] = 0;
+    gameArray[offset + (i-2)*10 + active[1]] = 0;
+    if (active === zShape || active === reversezShape) {
+      gameArray[offset + (i-2)*10 + active[2]] = 0;
+    }
+    //gameArray[offset + (i-2)*10 + 10] = 0;
+    //gameArray[offset + (i-2)*10 + 11] = 0;
+
+
+    if (cells[offset + i*10].style.backgroundColor == "green" || cells[offset + i*10].style.backgroundColor == "red" ||
+    cells[offset + i*10 + active[1]].style.backgroundColor == "green" || cells[offset + i*10 + active[1]].style.backgroundColor == "red" ||
+    cells[offset + i*10 + active[2]].style.backgroundColor == "green" || cells[offset + i*10 + active[2]].style.backgroundColor == "red" ||
+    cells[offset + i*10 + active[3]].style.backgroundColor == "green" || cells[offset + i*10 + active[3]].style.backgroundColor == "red") {
         i = 0;
         offset = 5;
+        active = change();
     }
 }
 
